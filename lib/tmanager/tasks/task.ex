f6eb7a76af91c10ca46c89c77703537beb2ebd9e
alesia.nastase <alesia.nastase>
2026-09-22 10:16:37 +0200

    Added TaskController

--- lib/tmanager/tasks/task.ex
+++ lib/tmanager/tasks/task.ex
@@ -2,6 +2,7 @@ defmodule Todolist.Tasks.Task do
   use Ecto.Schema
   import Ecto.Changeset
 
+  @derive {Jason.Encoder, only: [:id, :title, :description, :status, :user_id, :inserted_at, :updated_at]}
   schema "tasks" do
     field :status, :string
     field :description, :string
