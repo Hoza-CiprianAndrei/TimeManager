3f7c38b47b626293801fe12c59051fbc27cc6a0d
ciprian_andrei.hoza <ciprian_andrei.hoza@stud.acs.upb.ro>
2026-09-23 11:46:46 +0300

    Wrote de working_time_controller

--- lib/tmanager_web/controllers/working_time_controller.ex
+++ lib/tmanager_web/controllers/working_time_controller.ex
@@ -7,7 +7,7 @@ defmodule TodolistWeb.WorkingTimeController do
 
 
     def show(conn, %{"userID" => userid, "id" => id}) do
-        case Repo.get_by(WorkingTime, id : id, userID: userid) do
+        case Repo.get_by(WorkingTime, id: id, userID: userid) do
             nil ->
                 conn |> put_status(:not_found) |> json(%{error: "Working time not found for this user!"})
             working_time -> 
